8415f8415a8e3e163e70edc4d44c14a1d521740c
ParasTemkar <parastemkar0204@gmail.com>
2025-02-10 00:08:30 +0530

    Create 16 division inside main container.

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const container = document.createElement('div');
 body.append(container);
 container.classList.add('container'); 
 
+for (let i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    container.append(row);
+    row.classList.add('row');
+}
 
