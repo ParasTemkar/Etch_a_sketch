b453fb289d6fcc2bd50ae3a2e607f601b3160eb6
ParasTemkar <parastemkar0204@gmail.com>
2025-02-10 00:11:16 +0530

    Create 16 div inside each row making 16x16 grid.

--- script.js
+++ script.js
@@ -3,9 +3,16 @@ const container = document.createElement('div');
 body.append(container);
 container.classList.add('container'); 
 
+// Create 16x16 grid
 for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
     container.append(row);
     row.classList.add('row');
+    
+    for (let i = 0; i < 16; i++) {
+        const square = document.createElement('div');
+        row.append(square);
+        square.classList.add('square');
+    }
 }
 
